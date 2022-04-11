98860d7c608a8c05b8781d3ae394dc315c79aa76
IvanBarajas95 <iaovalle1@misena.edu.co>
2022-04-11 11:20:42 -0500

    "Se automatiza cierre de sesion"

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -11,11 +11,19 @@ public class Login extends Method {
     private WebElement password;
     @FindBy(xpath="//i[@class='fa fa-2x fa-sign-in']")
     private WebElement buttonlogin;
+    @FindBy(xpath="//a[@class='button secondary radius']")
+    private WebElement buttonlogout;
 
     public Login initlogin(){
         SEN_KEYS_INTERFACE.sendKeys(username,"tomsmith");
         SEN_KEYS_INTERFACE.sendKeys(password,"SuperSecretPassword!");
         UTILS_INTERFACE.clickElement(buttonlogin);
-        WAIT_INTERFACE.pause(5);
+        WAIT_INTERFACE.pause(3);
+    return this;}
+
+    public Login logoutsession(){
+        WAIT_INTERFACE.waitForElementToClickeable(10,5,buttonlogout);
+        UTILS_INTERFACE.clickElement(buttonlogout);
+        WAIT_INTERFACE.pause(3);
     return this;}
 }
---
+++ src/test/java/runner/Logout_Test.java
@@ -0,0 +1,14 @@
+package runner;
+
+import com.nivelics.commons.utils.Hooks;
+import org.testng.annotations.Test;
+import pages.Login;
+
+public class Logout_Test extends Hooks {
+    ThreadLocal<Login> cerrarsesion = ThreadLocal.withInitial(Login::new);
+    @Test
+    public void logoutsesion(){
+        cerrarsesion.get().initlogin();
+        cerrarsesion.get().logoutsession();
+    }
+}
